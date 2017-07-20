ac7a433a116f56c646ea7fe5d5f3d7106c1363e9
Maddie Zug <madelinezug@gmail.com>
2017-07-20 10:16:01 -0700

    updated graph colors

--- SIGSCE/cs60_barplot_proportional.R
+++ SIGSCE/cs60_barplot_proportional.R
@@ -23,6 +23,11 @@ df_combined$Major_group <- combineLevels(df_combined$Major_group, c("CSI", "CSM"
 df_combined$Major_group <- combineLevels(df_combined$Major_group, c("482", "518", "559", "MAB",
                                                                      "NON", "SDM"), "Don't Include")
 
+df_cs_or_not$Major_group <- combineLevels(df_combined$Major_group, c("Physics", "Math", "Math/Comp. Bio.", "Natural Sciences", "Econ", "Politics, IR, Philosophy", "Social Sciences", 
+                                                         "Art, Language, Media", "Independent Program"), "Not CS")
+df_cs_or_not$Major_group <- combineLevels(df_cs_or_not$Major_group, c("Engineering", "Computer Science"), "CS")
+df_cs_or_not$Major_group <- combineLevels(df_cs_or_not$Major_group, c("UND", "Don't Include"), "UND")
+
 #Take out CS, ENG, and UND majors
 df_no_cs <- subset(df_combined, (Major_group != "Engineering"))
 df_no_cs <- subset(df_no_cs, (Major_group != "Computer Science"))
