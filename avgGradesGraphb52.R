4860bbfcc1a0292171556655cccfcd45bf46638f
hhoffman14 <hannahoffman14@gmail.com>
2017-08-04 09:59:02 -0700

    removing repeated files

--- avgGradesGraphb52.R
+++
@@ -1,7 +0,0 @@
-means <- c(2.856, 2.623, 2.896)
-names <- c("green", "gold", "black")
-standardDevs <- c(.750, .848, 0.777)
-plotTop <- 4
-barCenters <- barplot(means, names.arg=names, col=c('green','orange','dimgray'), las=1, ylim=c(0,plotTop),
-                      main='Average Grades in Bio52 by CS5 Flavor',ylab = 'Grades on a 4-point Scale')
-arrows(barCenters, means-standardDevs, barCenters, means+standardDevs, lwd=2, angle=90, code=3)
